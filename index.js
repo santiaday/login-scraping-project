e578d87cbf19b172c13e3c56e691a8939ddc9cd8
Santiago Aday <santiagoaday7@gmail.com>
2023-02-17 12:54:25 -0500

    another commit

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const path = require('path');
 
-app.listen(3000);
+app.use(express.static('public'))
 
-app.use(express.static("public"));
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+})
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(__dirname, "public") });
-});
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
