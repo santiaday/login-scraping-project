d4395a71a95756404da119e10f94307a062abbd3
Santiago Aday <74830912+santiaday@users.noreply.github.com>
2023-02-28 10:37:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -64,136 +64,198 @@ app.get("/get-lead-information", (req, res) => {
     // Wait for the page to load and then print the page content
     await page.waitForNavigation();
     pageData = await page.evaluate(() => document.querySelector("*").outerHTML);
-    let leadDescription = (await page.evaluate(
-      () =>
-        document.getElementsByClassName("specsheet-intro ng-binding")[0]
-          .innerHTML
-    )) || "";
-    let companyName = (await page.evaluate(() =>
+    let leadDescription = await page.evaluate(() =>
+      document.getElementsByClassName("specsheet-intro ng-binding")[0]
+        .innerHTML === "undefined"
+        ? ""
+        : document.getElementsByClassName("specsheet-intro ng-binding")[0]
+            .innerHTML
+    );
+    let companyName = await page.evaluate(() =>
       document
         .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[0]
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let location = (await page.evaluate(() =>
-      document
-        .querySelectorAll(
-          "span[ng-bind-html='Lead.Referral.Match.Company.Location.Address | inlineAddress'] a"
-        )[0]
-        .innerHTML.trim()
-    )) || "";
-    let businessHours = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[0]
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let location = await page.evaluate(() =>
+      document.querySelectorAll(
+        "span[ng-bind-html='Lead.Referral.Match.Company.Location.Address | inlineAddress'] a"
+      )[0].innerHTML === "undefined"
+        ? ""
+        : document
+            .querySelectorAll(
+              "span[ng-bind-html='Lead.Referral.Match.Company.Location.Address | inlineAddress'] a"
+            )[0]
+            .innerHTML.trim()
+    );
+    let businessHours = await page.evaluate(() =>
       document
         .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[1]
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let interviewRecording = (await page.evaluate(
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[1]
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let interviewRecording = await page.evaluate(
       () => document.querySelectorAll(".specsheet-phone-recording")[0].href
-    )) || "";
-    let contactName = (await page.evaluate(
+    );
+    let contactName = await page.evaluate(
       () => document.querySelectorAll(".name.ng-binding")[0].innerHTML
-    )) || "";
-    let contactFirstName = (await page.evaluate(
+    );
+    let contactFirstName = await page.evaluate(
       () =>
         document.querySelectorAll(".name.ng-binding")[0].innerHTML.split(" ")[0]
-    )) || "";
-    let contactLastName = (await page.evaluate(
+    );
+    let contactLastName = await page.evaluate(
       () =>
         document.querySelectorAll(".name.ng-binding")[0].innerHTML.split(" ")[1]
-    )) || "";
-    let contactPosition = (await page.evaluate(
+    );
+    let contactPosition = await page.evaluate(
       () =>
         document
           .querySelectorAll(".job-title.ng-binding.ng-scope")[0]
           .innerHTML.split("\x3C")[0]
-    )) || "";
-    let contactEmail = (await page.evaluate(
+    );
+    let contactEmail = await page.evaluate(
       () =>
         document.querySelectorAll(".email.ng-binding.ng-scope a")[0].innerHTML
-    )) || "";
-    let contactPhone = (await page.evaluate(
+    );
+    let contactPhone = await page.evaluate(
       () =>
         document.querySelectorAll(".phone.ng-binding.ng-scope a")[0].innerHTML
-    )) || "";
-    let interviewedBy = (await page.evaluate(() =>
+    );
+    let interviewedBy = await page.evaluate(() =>
       document
         .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[2]
-        .innerHTML.split("</strong>")[1]
-        .trim()
-        .split(" \x3c")[0]
-        .trim()
-    )) || "";
-    let generalLocation = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[2]
+            .innerHTML.split("</strong>")[1]
+            .trim()
+            .split(" \x3c")[0] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[2]
+            .innerHTML.split("</strong>")[1]
+            .trim()
+            .split(" \x3c")[0]
+            .trim()
+    );
+    let generalLocation = await page.evaluate(() =>
       document
         .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[3]
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let industry = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[3]
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let industry = await page.evaluate(() =>
       document
         .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[4]
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let annualSales = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[4]
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let annualSales = await page.evaluate(() =>
       document
         .querySelector('li[ng-show="Lead.Specs.Company.AnnualSales"]')
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let numberOfEmployees = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelector('li[ng-show="Lead.Specs.Company.AnnualSales"]')
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let numberOfEmployees = await page.evaluate(() =>
       document
         .querySelector('li[ng-show="Lead.Specs.Company.Employees"]')
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let numberOfUsers = (await page.evaluate(
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelector('li[ng-show="Lead.Specs.Company.Employees"]')
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let numberOfUsers = await page.evaluate(
       () =>
         document.querySelector(
           'li[ng-show="Lead.Specs.ConcurrentUsers || Lead.Specs.NamedUsers"] span'
         ).innerHTML
-    )) || "";
-    let timeframe = (await page.evaluate(() =>
+    );
+    let timeframe = await page.evaluate(() =>
       document
         .querySelector('li[ng-show="Lead.Specs.Timeframe"]')
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let functionalityRequirements = (await page.evaluate(
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelector('li[ng-show="Lead.Specs.Timeframe"]')
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let functionalityRequirements = await page.evaluate(
       () =>
         document.querySelectorAll(
           ".specsheet-answers-applist-app-name.ng-binding"
         )[0].innerHTML
-    )) || "";
+    );
 
-    let spendingExpectations = (await page.evaluate(() =>
+    let spendingExpectations = await page.evaluate(() =>
       document
         .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[9]
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let spending3YearTCO = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[9]
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let spending3YearTCO = await page.evaluate(() =>
       document
         .querySelector('li[ng-if="Lead.Specs.Budget.Calculation"]')
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let currentSoftware = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelector('li[ng-if="Lead.Specs.Budget.Calculation"]')
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let currentSoftware = await page.evaluate(() =>
       document
         .querySelector(
           'li[ng-if="Lead.Specs.Infrastructure.CurrentSystem.Product.Name"]'
         )
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let deployment = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelector(
+              'li[ng-if="Lead.Specs.Infrastructure.CurrentSystem.Product.Name"]'
+            )
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let deployment = await page.evaluate(() =>
       document
         .querySelector('li[ng-if="Lead.Specs.Deployment"]')
-        .innerHTML.split("</strong>")[1]
-        .trim()
-    )) || "";
-    let interviewNotes = (await page.evaluate(() =>
+        .innerHTML.split("</strong>")[1] === "undefined"
+        ? ""
+        : document
+            .querySelector('li[ng-if="Lead.Specs.Deployment"]')
+            .innerHTML.split("</strong>")[1]
+            .trim()
+    );
+    let interviewNotes = await page.evaluate(() =>
       Array.from(
         document.querySelectorAll('div[ng-bind-html="Lead.Notes.Interview"] p')
       )
@@ -201,31 +263,39 @@ app.get("/get-lead-information", (req, res) => {
           return paragraph.innerText;
         })
         .join("")
-    )) || "";
-    let optionsRequested = (await page.evaluate(
+    );
+    let optionsRequested = await page.evaluate(
       () =>
         document.querySelector('ng-pluralize[count="Lead.Info.Slots.Max"]')
           .innerHTML
-    )) || "";
-    let industryExpertiseImportance = (await page.evaluate(() =>
+    );
+    let industryExpertiseImportance = await page.evaluate(() =>
       document
         .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[14]
-        .innerText.split("\n")[1]
-        .trim()
-    )) || "";
-    let localSupportImportance = (await page.evaluate(() =>
+        .innerText.split("\n")[1] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[14]
+            .innerText.split("\n")[1]
+            .trim()
+    );
+    let localSupportImportance = await page.evaluate(() =>
       document
         .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[15]
-        .innerText.split("\n")[1]
-        .trim()
-    )) || "";
-    let competition = (await page.evaluate(() =>
+        .innerText.split("\n")[1] === "undefined"
+        ? ""
+        : document
+            .querySelectorAll("ul.specsheet-answers-content li.ng-binding")[15]
+            .innerText.split("\n")[1]
+            .trim()
+    );
+    let competition = await page.evaluate(() =>
       Array.from(document.querySelectorAll("em.ng-binding"))
         .map((paragraph) => {
           return paragraph.innerText;
         })
         .join()
-    )) || "";
+    );
 
     // Close the browser window
     await browser.close();
