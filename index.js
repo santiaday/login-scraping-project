7f2eaafa57253d8b8467876a958d5e5ce02b75be
Santiago Aday <74830912+santiaday@users.noreply.github.com>
2023-06-06 15:59:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.listen(process.env.PORT || 3000, () => {
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://santiaday.github.io");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
