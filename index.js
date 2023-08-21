d5b1ed749b6b71f288483587cdc90d6b572fa41d
Santiago Aday <74830912+santiaday@users.noreply.github.com>
2023-08-21 12:57:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -35,11 +35,14 @@ app.listen(process.env.PORT || 3000, () => {
 
 app.use(express.static("public"));
 
+let chrome = {};
+let puppeteer;
+
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   chrome = require("chrome-aws-lambda");
   puppeteer = require("puppeteer-core");
 } else {
-  puppeteer = require("puppeteer-core");
+  puppeteer = require("puppeteer");
 }
 
 app.get("/get-lead-information", (req, res) => {
@@ -740,6 +743,16 @@ function camelize(str) {
 app.get("/changeCapterraBids", async (req, res) => {
   process.env.AWS_EXECUTION_ENV = "AWS_Lambda_nodejs14.x";
 
+  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: true,
+      ignoreHTTPSErrors: true,
+    };
+  }
+
     /*let options = {};
 
     if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
@@ -791,12 +804,7 @@ app.get("/changeCapterraBids", async (req, res) => {
 
 
 
-const browser = await puppeteer.launch({
-    args: chrome.args,
-    defaultViewport: chrome.defaultViewport,
-    executablePath: await chrome.executablePath(),
-    headless: chrome.headless,
-  });
+    let browser = await puppeteer.launch(options);
     const page = await browser.newPage();
 
     await page.goto(
